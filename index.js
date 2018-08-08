215ec8ed402a75d0b1699c7c45057f7e62d6a6da
Jian Ye <monian@momoko.cc>
2018-08-08 14:35:34 +0800

    增加重复定义判断

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const FastNodeWeiXin = require("./lib/common");
 // 模板消息接口
 FastNodeWeiXin.mixin(require("./lib/template"));
 
+// 消息事件匹配
+FastNodeWeiXin.mixin(require("./lib/match"));
+
+// 路由
+FastNodeWeiXin.mixin(require("./lib/router"));
+
 module.exports = FastNodeWeiXin;
